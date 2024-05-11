70e095e291359c61f707af7d44117147cd1e5234
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-05-11 17:41:39 +0900

    impl gess

--- main.jl
+++ main.jl
@@ -1,2 +1,25 @@
-file = ARGS[1]
-(@ccall "export.so".cmd_gat(file::Cstring)::Cstring)
+using IOCapture: IOCapture
+using TerminalPager: pager
+
+function gat(file::AbstractString)
+    (@ccall "export.so".cmd_gat(file::Cstring)::Cstring)
+    nothing
+end
+
+function gess(file::AbstractString)
+    c = IOCapture.capture() do
+        gat(file)
+    end
+
+    out = c.output
+    out |> pager
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    if length(ARGS) != 1
+        println("Usage: julia $(PROGRAM_FILE) <filename>")
+        exit(1)
+    end
+    file = ARGS[begin]
+    gat(file)
+end
